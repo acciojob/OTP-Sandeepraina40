831d13eaef8e451e1056213100e2c20d0aa2a5d7
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-08-30 20:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    // Allow only numbers
+    e.target.value = e.target.value.replace(/[^0-9]/g, '');
+    if (e.target.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace' && !input.value && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
+});
+
+// Focus the first box on page load
+inputs[0].focus();
